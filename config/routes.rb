18ef92585ecf8e7b888f6fcec3737f553df866d3
GDawg78 <me@gavinsimms.com>
2020-11-10 11:24:32 +1100

    changed route to be top of the list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'items#index'
   resources :items
   devise_for :users, :controllers => {:registrations => "registrations"}
-  root to: 'items#index'
   resources :states
   resources :profiles
 end
