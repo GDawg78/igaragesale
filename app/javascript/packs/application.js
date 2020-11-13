1e55f026467502a9b53ff22c55e935472f2eecc2
GDawg78 <me@gavinsimms.com>
2020-11-13 12:42:56 +1100

    Janel fixed my issue with delete verb not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import 'bootstrap'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import "bootstrap"
-import "../stylesheets/application"
+require("jquery")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,3 +15,10 @@ import "../stylesheets/application"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+//= require jquery3
+//= require popper
+//= require bootstrap
